53273bc5ede739bb6319b42d985fe1b07841cec6
樊小聪 <1016697223@qq.com>
2017-09-28 14:37:14 +0800

    创建项目

---
+++ XCMacrosExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XCMacrosExample
+//
+//  Created by 樊小聪 on 2017/9/28.
+//  Copyright © 2017年 樊小聪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XCMacrosExampleTests/XCMacrosExampleTests.m
@@ -0,0 +1,39 @@
+//
+//  XCMacrosExampleTests.m
+//  XCMacrosExampleTests
+//
+//  Created by 樊小聪 on 2017/9/28.
+//  Copyright © 2017年 樊小聪. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XCMacrosExampleTests : XCTestCase
+
+@end
+
+@implementation XCMacrosExampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
